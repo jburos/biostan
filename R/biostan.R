bf8450ef0994c132a5fe3b8d16788470d233cb41
Jacqueline Buros Novik <jackinovik@gmail.com>
2016-06-15 11:19:45 -0400

    added package description & preliminary dependencies

---
+++ R/biostan.R
@@ -0,0 +1,16 @@
+#' biostan: Companion package to BioC 2016 Workshop entitled "Introduction to Bayesian Inference using Stan with Applications to Cancer Genomics"
+#'
+#' This workshop will provide a background and introduction to Bayesian analysis using Stan and R interfaces to Stan. We will then work through 1-2 example applications in the area of cancer genomics.
+#'
+#' [Stan](http://mc-stan.org/) is a modern probabilistic programming language implementing full Bayesian
+#'  statistical inference with MCMC sampling (NUTS, HMC), approximate Bayesian inference with
+#'  variational inference (ADVI), and penalized maximum likelihood estimation with optimization (L-BFGS).
+#'
+#' This package contains vignettes & utility functions in support of the BioC 2016 workshop.
+#'
+#' @section utilities
+#'
+#' @section vignettes
+#'
+#' @import dplyr, tidyr, httr, readr, cgdsr, dplyr
+NULL
